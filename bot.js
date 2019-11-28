f5eb17f64289d8ca7bd897b3f4326a9a2da20f1b
dsnidr <6261352+dsnidr@users.noreply.github.com>
2019-11-28 14:54:28 -0500

    :bug: Fixing unexpected commit behaviour

--- bot.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-module.exports = client;
