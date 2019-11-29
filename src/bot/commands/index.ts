d7d5fbd1a5342a554e5bdd93966f0a065e08c092
dsnidr <6261352+dsnidr@users.noreply.github.com>
2019-11-28 22:36:29 -0500

    :construction: Started work on the subscription command

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,10 +1,14 @@
 import { Message, TextChannel } from "discord.js";
 import clear from "./clear";
+import subscribe from "./subscribe";
 
 export default (command: string, args: string[], message: Message) => {
 	switch (command) {
 		case "clear":
 			clear(message.channel as TextChannel);
+			break;
+		case "subscribe":
+			subscribe(message, args);
 			break;
 	}
 };
