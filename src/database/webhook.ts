ba3fc9650db057cbea3119655e7c5a593ace0fe6
dsnidr <6261352+dsnidr@users.noreply.github.com>
2019-11-30 01:58:27 -0500

    :card_file_box: Added Webhook database skeleton functions

---
+++ src/database/webhook.ts
@@ -0,0 +1,16 @@
+export const createWebhook = (
+	guildId: string,
+	channelId: string,
+	repoUrl: string,
+	secret: string
+) => {
+	// Placeholder
+};
+
+export const getWebhook = (guildId: string, channelId: string) => {
+	// Placeholder
+};
+
+export const deleteWebhook = (webhookId: number) => {
+	// Placeholder
+};
