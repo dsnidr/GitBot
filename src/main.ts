77ccacbda98f354725f9fc379683fc2941077242
dsnidr <6261352+dsnidr@users.noreply.github.com>
2019-11-30 13:59:52 -0500

    :construction: Expanded upon OAuth state functionality
    Before we only kept track of the repoUrl in the state, but now we also need to keep track of the guildId and channelId. They are now passed along in an encoded JSON object.

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { Message } from "discord.js";
 import bot from "./bot";
 import server from "./server";
 import keys from "./config/keys";
-import isGitHubUrlBase64 from "./helpers/isGitHubUrlBase64";
+import isGitHubUrl from "./helpers/isGitHubUrl";
 import createWebhook from "./helpers/createWebhook";
 
 import handleCommand from "./bot/commands";
+import { insertWebhook } from "./database/webhook";
 
 bot.on("message", (message: Message) => {
 	let messageString: string = message.content;
@@ -41,7 +42,7 @@ server.post("/webhook", (req, res) => {
 	const body = req.body;
 
 	// If no payload was provided, drop the request.
-	if (!body) {
+	/*if (!body) {
 		return;
 	}
 
@@ -69,18 +70,44 @@ server.post("/webhook", (req, res) => {
 	// If an invalid hash was provided, drop the request.
 	if (!hashesMatch) {
 		return;
-	}
+	}*/
 
 	res.status(200).json({
 		success: true
 	});
 });
 
+interface RepoState {
+	repoUrl: string;
+	guildId: string;
+	channelId: string;
+}
+
 server.get("/auth/github", (req, res) => {
-	const encodedRepo = req.query.repo;
+	const encodedState: string = req.query.state;
+	const decodedState = Buffer.from(encodedState, "base64").toString("ascii");
+
+	let state: RepoState = undefined;
+
+	try {
+		state = JSON.parse(decodedState) as RepoState;
+	} catch (e) {
+		return res.send(`
+		<!DOCTYTPE html>
+		<html>
+			<body>
+				<h1>Something went wrong</h1>
+				<p>If you're here by mistake, you may close this tab.</p>
+				<p>If you didn't expect this error, please reclick the link sent to you by GitBot</p>
+			</body>
+		</html>`);
+	}
+
+	// TODO: check if webhook already exists
+	// TODO: make sure that guildId and channelId are valid IDs
 
 	// Ensure that a repo parameter exists on the query string
-	if (!isGitHubUrlBase64(encodedRepo)) {
+	if (!isGitHubUrl(state.repoUrl)) {
 		return res.send(`
 		<!DOCTYTPE html>
 		<html>
@@ -93,14 +120,34 @@ server.get("/auth/github", (req, res) => {
 	}
 
 	res.redirect(
-		`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${keys.GITHUB_CLIENT_ID}&state=${encodedRepo}`
+		`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${keys.GITHUB_CLIENT_ID}&state=${state.repoUrl}`
 	);
 });
 
 server.get("/auth/github/callback", (req, res) => {
-	const encodedRepo = req.query.state;
+	// TODO: Move the below code into a validation function to avoid duplicate code
 
-	if (!isGitHubUrlBase64(encodedRepo)) {
+	const encodedState = req.query.state;
+
+	const decodedState = Buffer.from(encodedState, "base64").toString("ascii");
+
+	let state: RepoState = undefined;
+
+	try {
+		state = JSON.parse(decodedState) as RepoState;
+	} catch (e) {
+		return res.send(`
+		<!DOCTYTPE html>
+		<html>
+			<body>
+				<h1>Something went wrong</h1>
+				<p>If you're here by mistake, you may close this tab.</p>
+				<p>If you didn't expect this error, please reclick the link sent to you by GitBot</p>
+			</body>
+		</html>`);
+	}
+
+	if (!isGitHubUrl(state.repoUrl)) {
 		return res.send(`
 		<!DOCTYTPE html>
 		<html>
@@ -125,11 +172,8 @@ server.get("/auth/github/callback", (req, res) => {
 				accept: "application/json"
 			}
 		},
-		(err, r, b) => {
+		async (err, r, b) => {
 			const body = JSON.parse(b);
-
-			// Decode the repo url
-			const repoUrl = Buffer.from(encodedRepo, "base64").toString("ascii");
 
 			// Generate a pseudo random secret key for the webhook
 			const secret = crypto.randomBytes(20).toString("hex");
@@ -138,11 +182,43 @@ server.get("/auth/github/callback", (req, res) => {
 			// TODO: This secret MUST be stored in a database //
 			////////////////////////////////////////////////////
 
-			console.log("SECRET", secret);
+			const statusCode = await createWebhook(state.repoUrl, body.access_token, secret);
 
-			createWebhook(repoUrl, body.access_token, secret).then(result => {
-				res.send(result);
-			});
+			if (statusCode === 201) {
+			}
+
+			// TODO: Create actual templates so we don't need to use these ugly html literal strings
+
+			// 404 = Not Found
+			if (statusCode === 404) {
+				return res.send(`
+				<html>
+					<body>
+						<h1>That repository could not be found</h1>
+					</body>
+				</html>`);
+			}
+
+			// 201 = Created
+			else if (statusCode !== 201) {
+				return res.send(`
+				<html>
+					<body>
+						<h1>Something went wrong</h1>
+						<p>We were unable to create a webhook to that repository. Please try again later</p>
+					</body>
+				</html>`);
+			}
+
+			// Otherwise, everything was successful
+			else {
+				return res.send(`
+				<html>
+					<body>
+						<h1>Webhook Created</h1>
+					</body>
+				</html>`);
+			}
 		}
 	);
 });
