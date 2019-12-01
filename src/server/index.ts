6fb7fc9779054c26cd7d60f1ee691019e58ec0bd
Sniddunc <sniddunc2@gmail.com>
2019-12-01 00:48:39 -0500

    :lipstick: Implemented EJS rendering and SCSS styling
    Also, our routes were restructured into their own files.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import rateLimit from "express-rate-limit";
+import path from "path";
+import authRouter from "./routes/auth";
+import webhookRouter from "./routes/webhook";
 
 const server = express();
 
@@ -9,8 +12,23 @@ const limiter = rateLimit({
 	max: 100
 });
 
+server.set("view engine", "ejs");
+server.set("views", __dirname + "/views");
+
+console.log(path.join(__dirname + "/../public"));
+
+server.use(express.static(path.join(__dirname + "/.." + "/public")));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(limiter);
+
+// Implement our custom routes
+server.use("/auth", authRouter);
+server.use("/webhook", webhookRouter);
+
+// Root route
+server.get("/", (req, res) => {
+	res.render("home");
+});
 
 export default server;
